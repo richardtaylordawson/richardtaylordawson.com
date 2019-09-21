65f509617b1965324b2fb40aa4247f4143ceb3ad
Taylor Dawson <taylor.dawson@clearlink.com>
2019-09-21 15:48:08 -0600

    Fixing website url that is redirecting sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://www.richarddawson.codes',
-        sitemap: 'https://www.richarddawson.codes/sitemap.xml',
+        host: 'https://richarddawson.codes',
+        sitemap: 'https://richarddawson.codes/sitemap.xml',
         policy: [{ userAgent: '*', allow: '/', }]
       }
     },
