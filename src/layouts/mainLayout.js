d4f194c2603146c8d9d60608603a2cef0e2b32a3
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2019-11-23 21:44:42 -0700

    test

--- src/layouts/mainLayout.js
+++ src/layouts/mainLayout.js
@@ -1,17 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { MenuProvider, ThemeProvider } from "../context/"
+import { contextWrapper } from "./contextWrapper"
 import { Header, Footer, GlobalTheme } from "./../components/"
 
 export const MainLayout = ({ children }) => (
-  <ThemeProvider>
-    <MenuProvider>
-      <GlobalTheme />
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </MenuProvider>
-  </ThemeProvider>
+  <contextWrapper>
+    <GlobalTheme />
+    <Header />
+    <main>{children}</main>
+    <Footer />
+  </contextWrapper>
 )
 
 MainLayout.propTypes = {
