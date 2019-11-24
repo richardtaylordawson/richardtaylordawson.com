631db13c5176cd316e0edb16efc653a5c134d3b3
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2019-11-23 22:14:52 -0700

    test

--- src/context/theme/theme-context.js
+++ src/context/theme/theme-context.js
@@ -16,31 +16,31 @@ const useTheme = () => {
 const ThemeProvider = props => {
   let themeToUse = Themes.light
 
-  if (typeof window !== "undefined") {
-    const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches
-    const isLightMode = window.matchMedia("(prefers-color-scheme: light)").matches
-    const isNotSpecified = window.matchMedia("(prefers-color-scheme: no-preference)").matches
-    const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified
+  // if (typeof window !== "undefined") {
+  //   const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches
+  //   const isLightMode = window.matchMedia("(prefers-color-scheme: light)").matches
+  //   const isNotSpecified = window.matchMedia("(prefers-color-scheme: no-preference)").matches
+  //   const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified
 
-    if (isDarkMode) themeToUse = Themes.dark
-    if (isLightMode) themeToUse = Themes.light
-    if (isNotSpecified || hasNoSupport) {
-      const now = new Date()
-      const hour = now.getHours()
+  //   if (isDarkMode) themeToUse = Themes.dark
+  //   if (isLightMode) themeToUse = Themes.light
+  //   if (isNotSpecified || hasNoSupport) {
+  //     const now = new Date()
+  //     const hour = now.getHours()
 
-      if (hour < 4 || hour >= 16) {
-        themeToUse = Themes.dark
-      }
-    }
-  }
+  //     if (hour < 4 || hour >= 16) {
+  //       themeToUse = Themes.dark
+  //     }
+  //   }
+  // }
 
   const [theme, setTheme] = useState(themeToUse)
   const value = useMemo(() => [theme, setTheme], [theme])
 
-  if (typeof window !== "undefined") {
-    window.matchMedia("(prefers-color-scheme: dark)").addListener(e => e.matches && setTheme(Themes.dark))
-    window.matchMedia("(prefers-color-scheme: light)").addListener(e => e.matches && setTheme(Themes.light))
-  }
+  // if (typeof window !== "undefined") {
+  //   window.matchMedia("(prefers-color-scheme: dark)").addListener(e => e.matches && setTheme(Themes.dark))
+  //   window.matchMedia("(prefers-color-scheme: light)").addListener(e => e.matches && setTheme(Themes.light))
+  // }
 
   return <ThemeContext.Provider value={value} {...props} />
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,26 +2,20 @@ import React from "react"
 import styled from "@emotion/styled"
 import { MainLayout } from "./../layouts/mainLayout"
 import { H1, P, Container, SEO } from "../components"
-import { useTheme, useMenu } from "./../context/"
 
-const NotFound = () => {
-  const [theme] = useTheme()
-  const [menu] = useMenu()
-
-  return (
-    <MainLayout>
-      <SEO
-        title="Not Found"
-      />
-      <Wrapper>
-        <Container>
-          <H1>NOT FOUND</H1>
-          <P>Unfortunately that's not a page...</P>
-        </Container>
-      </Wrapper>
-    </MainLayout>
-  )
-}
+const NotFound = () => (
+  <MainLayout>
+    <SEO
+      title="Not Found"
+    />
+    <Wrapper>
+      <Container>
+        <H1>NOT FOUND</H1>
+        <P>Unfortunately that's not a page...</P>
+      </Container>
+    </Wrapper>
+  </MainLayout>
+)
 
 const Wrapper = styled.div`
   height: calc(100vh - 120px);
