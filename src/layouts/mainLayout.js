29ed1b9621921b049b8e576308306bbce050cf72
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2019-11-23 21:51:41 -0700

    removing wrapper in main layout since it's in browser file

--- src/layouts/mainLayout.js
+++ src/layouts/mainLayout.js
@@ -1,17 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { MenuProvider, ThemeProvider } from "../context/"
 import { Header, Footer, GlobalTheme } from "./../components/"
 
 export const MainLayout = ({ children }) => (
-  <ThemeProvider>
-    <MenuProvider>
-      <GlobalTheme />
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </MenuProvider>
-  </ThemeProvider>
+  <>
+    <GlobalTheme />
+    <Header />
+    <main>{children}</main>
+    <Footer />
+  </>
 )
 
 MainLayout.propTypes = {
