e0b9d76b85fdbacc33e41f21332faa05612130e9
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2019-11-23 22:01:09 -0700

    test

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,20 +2,26 @@ import React from "react"
 import styled from "@emotion/styled"
 import { MainLayout } from "./../layouts/mainLayout"
 import { H1, P, Container, SEO } from "../components"
+import { useTheme, useMenu } from "./../../context/"
 
-const NotFound = () => (
-  <MainLayout>
-    <SEO
-      title="Not Found"
-    />
-    <Wrapper>
-      <Container>
-        <H1>NOT FOUND</H1>
-        <P>Unfortunately that's not a page...</P>
-      </Container>
-    </Wrapper>
-  </MainLayout>
-)
+const NotFound = () => {
+  const [theme] = useTheme()
+  const [menu] = useMenu()
+
+  return (
+    <MainLayout>
+      <SEO
+        title="Not Found"
+      />
+      <Wrapper>
+        <Container>
+          <H1>NOT FOUND</H1>
+          <P>Unfortunately that's not a page...</P>
+        </Container>
+      </Wrapper>
+    </MainLayout>
+  )
+}
 
 const Wrapper = styled.div`
   height: calc(100vh - 120px);
