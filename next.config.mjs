c8d04a251292283ec06ffb0d4098f704635da0aa
Taylor Dawson <richard.taylor.dawson@gmail.com>
2026-01-25 13:46:08 -0700

    feat: updating

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,10 @@
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
-import rehypePrism from '@mapbox/rehype-prism'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx'],
+  pageExtensions: ['js', 'jsx'],
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true,
   },
 }
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig
