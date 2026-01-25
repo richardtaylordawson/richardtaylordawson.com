2c8d5adc69b637306451f983b850495c2e772e7c
Taylor Dawson <richard.taylor.dawson@gmail.com>
2026-01-25 15:07:14 -0700

    feat: updating copy

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -68,8 +68,13 @@ export default function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Taylor Dawson
+            Richard Dawson
           </h1>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+            I’m Richard, a software engineer and entrepreneur based near Salt
+            Lake City. I'm a highly motivated, hard working engineer that has a
+            knack for finding simple solutions to complex issues
+          </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
               href="https://www.instagram.com/rtdaws003/"
