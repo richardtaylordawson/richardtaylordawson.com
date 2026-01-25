21b9e288e5ae151c01752e521ce625d9cbdf166a
Taylor Dawson <richard.taylor.dawson@gmail.com>
2026-01-25 15:14:05 -0700

    feat: adding new favicon and removing unused code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -96,18 +96,6 @@ function NavItem({ href, children }) {
   )
 }
 
-function DesktopNavigation(props) {
-  return (
-    <nav {...props}>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about">About</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/uses">Uses</NavItem>
-      </ul>
-    </nav>
-  )
-}
-
 function ModeToggle() {
   function disableTransitionsTemporarily() {
     document.documentElement.classList.add('[&_*]:!transition-none')
@@ -344,10 +332,6 @@ export function Header() {
                   </AvatarContainer>
                 )}
               </div>
-              {/* <div className="flex flex-1 justify-end md:justify-center">
-                <MobileNavigation className="pointer-events-auto md:hidden" />
-                <DesktopNavigation className="pointer-events-auto hidden md:block" />
-              </div> */}
               <div className="flex justify-end md:flex-1">
                 <div className="pointer-events-auto">
                   <ModeToggle />
