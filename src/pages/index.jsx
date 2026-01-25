c5be82c26b16945678a5b2fac7760c2569687096
Taylor Dawson <richard.taylor.dawson@gmail.com>
2026-01-25 15:08:01 -0700

    feat: updating title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -68,7 +68,7 @@ export default function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Richard Dawson
+            Software Engineer
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Richard, a software engineer and entrepreneur based near Salt
