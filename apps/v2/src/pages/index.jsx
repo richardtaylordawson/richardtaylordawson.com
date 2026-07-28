01f573e2f343137d07502fb1c2a2218581f1ded3
Taylor Dawson <richard.taylor.dawson@gmail.com>
2026-07-28 08:37:40 -0600

    Make v2 site indexable

--- apps/v2/src/pages/index.jsx
+++ apps/v2/src/pages/index.jsx
@@ -8,7 +8,7 @@ import { Resume } from '@/components/Resume'
 import { SocialLink } from '@/components/SocialLink'
 import { Github, LinkedIn, LinkIcon } from '@/components/Icons'
 
-const siteUrl = 'https://www.richardtaylordawson.com'
+const siteUrl = 'https://v2.richardtaylordawson.com'
 
 const projects = [
   {
@@ -78,7 +78,6 @@ export default function Home() {
           Lake City. I'm a highly motivated, hard working engineer that has a
           knack for finding simple solutions to complex issues"
         />
-        <meta name="robots" content="noindex,follow" />
         <link rel="canonical" href={siteUrl} />
         <meta property="og:url" content={siteUrl} />
       </Head>
